f0b9c1afa91b231279e878e07bc0163b93059af1
Femi <femi96@mit.edu>
2018-06-24 10:34:45 -0400

    Add Plant base class

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Plant : Unit {
+  // Game controller that handles a plant's state and behavior
+
+  public GardenBoard board;
+  public GameObject model;
+
+  public float timeActive = 0f;
+
+  public override void Awake() {
+    base.Awake();
+    board = garden.GetBoard();
+    model = transform.Find("Model").gameObject;
+  }
+
+  void Update() {
+    timeActive += Time.deltaTime;
+
+    PlantBehavior();
+  }
+
+  public abstract void PlantBehavior();
+
+  public void Die() {
+    Destroy(gameObject);
+  }
+}
