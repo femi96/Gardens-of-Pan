f180f01f991bcc0f449a8c681df3054f01a50a46
Femi <femi96@mit.edu>
2018-07-22 19:30:52 -0700

    Fix MonsterSave spec

--- Assets/Scripts/MonsterSave.cs
+++ Assets/Scripts/MonsterSave.cs
@@ -11,7 +11,7 @@ public class MonsterSave : UnitSave {
   // public MonsterBehaviour[] behaviours = new MonsterBehaviour[0];
   public bool currentBehaviourDone = true;
 
-  // Creates MonsterSave with for a given monster
+  // Creates MonsterSave for a given monster
   public MonsterSave(Monster m) : base(m) {
     owned = m.owned;
     // currentBehaviour = m.currentBehaviour;
