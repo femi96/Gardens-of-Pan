bc57f88cd1bef12bad9a0b0f461156dffd656f4f
Femi Oladipupo <Femi@DESKTOP-D8TQN6P.mit.edu>
2018-11-22 19:03:57 -0500

    Add MonsterAction ActionPetted

---
+++ Assets/Scripts/MonsterAI/Actions/ActionPetted.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ActionPetted : MonsterAction {
+  // AI controller scripting monster behaviour
+  //  Monsters releases hearts and increases happiness
+
+  private float petHappiness;
+
+  public ActionPetted(float petHappiness) {
+    this.petHappiness = petHappiness;
+  }
+
+  public override void SetupAction(MonsterBehaviour behaviour, Monster monster) {}
+
+  public override void Act(MonsterBehaviour behaviour, Monster monster) {
+
+    // Setup necessary variables
+    Garden garden = monster.garden;
+
+    // Increase happiness
+    monster.happyInternal = petHappiness;
+
+    // Release hearts
+    garden = garden;
+
+    // End behavior
+    monster.EndBehaviour();
+  }
+}
